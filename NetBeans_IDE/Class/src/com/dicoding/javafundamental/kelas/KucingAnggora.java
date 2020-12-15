4fb8ec8062063d8a671e3819718abe93111d7381
jekitut <adklmn.irul@gmail.com>
2020-12-15 11:03:48 +0700

    Dicoding - Java

---
+++ NetBeans_IDE/Class/src/com/dicoding/javafundamental/kelas/KucingAnggora.java
@@ -0,0 +1,13 @@
+package com.dicoding.javafundamental.kelas;
+
+public class KucingAnggora {
+    
+    String namaSaya, jurusanSaya;
+    int nimSaya;
+    
+    public KucingAnggora(String nama, String jurusan, int nim) {
+        namaSaya = nama;
+        nimSaya = nim;
+        jurusanSaya = jurusan;
+    }
+}
